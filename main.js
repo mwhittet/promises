db4498204fa23caa720cd902f9f4a1b7cfcfe567
Michael Whittet <5453237+mwhittet@users.noreply.github.com>
2016-10-18 18:00:03 +0100

    Project WIP using the Spotify API.

--- main.js
+++ main.js
@@ -1,45 +1,15 @@
-function getSalary(salary) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(salary);
-    }, 1000);
+fetch('https://api.spotify.com/v1/search?q=michael+jackson&type=album')
+  .then(response => response.json())
+  .then(data => {
+    const uris = data.albums.items.map(item => {
+      return item.uri;
+    }).slice(0, 5);
+    
+    const tasks = uris.map(uri => fetch('https://api.spotify.com/v1/albums/' + uri.split(':')[2]));
+    return Promise.all(tasks);
+
+  }).then(responses => {
+    return Promise.all(responses.map(response => response.json()));
+  }).then(albums => {
+    console.log(albums);
   });
-}
-
-function getSalarySum() {
-  return Promise.all([
-    getSalary(10000),
-    getSalary(20000),
-    getSalary(40000),
-    getSalary(80000),
-    getSalary(160000),
-    getSalary(320000),
-  ]).then(salaries => {
-    return salaries.reduce((prev, cur) => prev + cur, 0);
-  });
-}
-
-
-function subtractTax(salary) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(salary * 0.75);
-    }, 200);
-  });
-}
-
-function subtractRent(salary) {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve(salary - 5000);
-    }, 200);
-  });
-}
-
-function getDisposableIncome(callback) {
-  return getSalarySum().then(subtractTax).then(subtractRent);
-}
-
-getDisposableIncome().then(disposable => {
-  console.log(disposable);
-});
