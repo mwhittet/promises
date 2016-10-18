cac249b74048ddf0f49a2bbf7c2ccb44105c28ce
Michael Whittet <5453237+mwhittet@users.noreply.github.com>
2016-10-18 17:26:51 +0100

    Initial promise construction.

--- main.js
+++ main.js
@@ -1,31 +1,38 @@
-function getSalary(callback) {
-  setTimeout(() => {
-    callback(33000);
-  }, 1000);
-}
-
-function subtractTax(salary, callback) {
-  setTimeout(() => {
-    callback(salary * 0.75);
-  }, 1000);
-}
-
-function subtractRent(salary, callback) {
-  setTimeout(() => {
-    callback(salary - 5000);
-  }, 1000);
-}
-
-function getDisposableIncome(callback) {
-  getSalary(salary_1 => {
-    subtractTax(salary_1, salary_2 => {
-      subtractRent(salary_2, salary_3 => {
-        callback(salary_3);
-      });
-    });
+function getSalary() {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(33000);
+    }, 1000);
   });
 }
 
-getDisposableIncome(disposable => {
+function subtractTax(salary) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(salary * 0.75);
+    }, 1000);
+  });
+}
+
+function subtractRent(salary) {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(salary - 5000);
+    }, 1000);
+  });
+}
+
+function getDisposableIncome(callback) {
+  const salaryPromise = getSalary();
+  const taxPromise = salaryPromise.then(salary_1 => {
+    return subtractTax(salary_1);
+  });
+  const rentPromise = taxPromise.then(salary_2 => {
+    return subtractRent(salary_2);
+  });
+  return rentPromise;
+}
+
+getDisposableIncome().then(disposable => {
   console.log(disposable);
 });
