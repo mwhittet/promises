b57e349563db7eb5771239702b8f217a7bd54dd4
Michael Whittet <5453237+mwhittet@users.noreply.github.com>
2016-10-18 17:16:54 +0100

    Callbacks.

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
-function getSalary() {
-  return 33000;
+function getSalary(callback) {
+  setTimeout(() => {
+    callback(33000);
+  }, 1000);
 }
 
-function subtractTax(salary) {
-  return salary * 0.75;
+function subtractTax(salary, callback) {
+  setTimeout(() => {
+    callback(salary * 0.75);
+  }, 1000);
 }
 
-function subtractRent(salary) {
-  return salary - 5000;
+function subtractRent(salary, callback) {
+  setTimeout(() => {
+    callback(salary - 5000);
+  }, 1000);
 }
 
-function getDisposableIncome() {
-  let salary = getSalary();
-  salary = subtractTax(salary);
-  salary = subtractRent(salary);
-  return salary;
+function getDisposableIncome(callback) {
+  getSalary(salary_1 => {
+    subtractTax(salary_1, salary_2 => {
+      subtractRent(salary_2, salary_3 => {
+        callback(salary_3);
+      });
+    });
+  });
 }
 
-console.log(getDisposableIncome());
+getDisposableIncome(disposable => {
+  console.log(disposable);
+});
