587078f1cd6d8d71def7e9e20ffcdbb0013352d1
Michael Whittet <5453237+mwhittet@users.noreply.github.com>
2016-10-19 11:40:21 +0100

    Complex promise chain which is easier to read then multiple callback functions.

--- main.js
+++ main.js
@@ -7,9 +7,28 @@ fetch('https://api.spotify.com/v1/search?q=michael+jackson&type=album')
     
     const tasks = uris.map(uri => fetch('https://api.spotify.com/v1/albums/' + uri.split(':')[2]));
     return Promise.all(tasks);
-
   }).then(responses => {
     return Promise.all(responses.map(response => response.json()));
   }).then(albums => {
-    console.log(albums);
+    return Promise.all(albums.map(album => fetch(album.images[0].url)));
+  }).then(responses => {
+    return Promise.all(responses.map(response => response.blob()));
+  }).then(blobs => {
+    return blobs.map(blob => URL.createObjectURL(blob));
+  }).then(urls => {
+    return Promise.all(
+      urls.map(url => {
+        return new Promise(resolve => {
+          const image = new Image();
+          image.addEventListener('load', () => {
+            resolve(image);
+          });
+          image.src = url;
+        });
+      })
+    );
+  }).then(images => {
+    images.forEach(image => {
+      document.body.appendChild(image);
+    });
   });
